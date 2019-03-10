8dd105160678fc431bb864cd3bb9df3362b43178
b-masuhr <41358153+b-masuhr@users.noreply.github.com>
2019-03-10 20:22:07 +0100

    Update ES6-Generators.js

--- ES6-Generators.js
+++ ES6-Generators.js
@@ -4,31 +4,32 @@ function *foo() {
   	yield "foo";
 	console.log(x);
 }
-
-console.log(foo().next());
-console.log(foo().next());
-> 1
-> Object { value: "foo", done: false }
-> 1
-> Object { value: "foo", done: false }
+console.log(foo().next()); // 1, // Object { value: "foo", done: false }
+console.log(foo().next()); // 1, // Object { value: "foo", done: false }
 
 var it = foo();
-console.log(it.next());
-console.log(it.next());
-> 1
-> Object { value: "foo", done: false }
-> 1
-> Object { value: undefined, done: true }
+console.log(it.next()); // 1, // Object { value: "foo", done: false }
+console.log(it.next()); // 1, // Object { value: undefined, done: true }
 
 function *foo2() {
     yield 1;
     yield 2;
 }
 var it2 = foo2();
-console.log(it2.next());
-console.log(it2.next());
-console.log(it2.next());
-> Object { value: undefined, done: true }
-> Object { value: 1, done: false }
-> Object { value: 2, done: false }
-> Object { value: undefined, done: true }
+console.log(it2.next()); // Object { value: 1, done: false }
+console.log(it2.next()); // Object { value: 2, done: false }
+console.log(it2.next()); // Object { value: undefined, done: true }
+
+for (var v of foo2()) {
+    console.log( v ); // 1, // 2
+}
+
+function *foo3(x) {
+    var y = 2 * (yield (x + 1));
+    var z = yield (y / 3);
+    return (x + y + z);
+}
+var it = foo3( 5 );
+console.log( it.next() );       // { value:6, done:false }
+console.log( it.next( 12 ) );   // { value:8, done:false }
+console.log( it.next( 13 ) );   // { value:42, done:true }
